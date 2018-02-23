2b66663e6e57f4c5511f9014acd62f2c391f5190
navib <ivan.baena.dev@gmail.com>
2018-02-23 11:18:15 -0500

    push to heroku

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,13 @@ const Footer = () => {
   return (
     <footer className="footer-wrapper">
       <div className="container flex-footer">
-        <h1>NAME PERSON</h1>
+        <h1>Justin C. Schilling</h1>
+        <ul>
+          <li><a href="#"><span aria-hidden="true" className="icon-home"></span></a></li>
+          <li><a href="#"><span aria-hidden="true" className="icon-home"></span></a></li>
+          <li><a href="#"><span aria-hidden="true" className="icon-home"></span></a></li>
+          <li><a href="#"><span aria-hidden="true" className="icon-home"></span></a></li>
+        </ul>
       </div>
     </footer>
   )
